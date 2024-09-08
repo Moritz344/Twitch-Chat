07e0b1874116f88fe1286fd8047f3ef33e9144b2
root <root@moritz>
2024-09-08 20:26:36 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from twitchio.ext import commands
+from dotenv import load_dotenv
+import os
+from termcolor import colored
+import random
+
+load_dotenv("secret.env")
+Twitch_token = os.getenv("TOKEN")
+channel = "ThePrimeagen"
+
+
+
+class Chat(commands.Bot):
+    def __init__(self):
+        super().__init__(token=Twitch_token,prefix="!",initial_channels=[channel])
+
+    async def event_ready(self):
+        print(colored(f"Bot ist bereit und verbunden mit {self.nick} im Kanal {channel}","yellow")) # Bot ist aktiv
+        
+
+    async def event_message(self,message): # prüft Nachrichten
+        if message.author.name.lower() == self.nick.lower():
+            return
+        farben_choice = ["red","green","cyan","blue","magenta"]
+        self.farben = random.choice(farben_choice)
+
+        if message.author.name == "nightbot":
+            self.farben = "white"
+        if message.author.name == "streamelements":
+            self.farben = "white"
+
+        color_message = colored(f"{message.author.name}",self.farben)
+        print(f"{color_message}: {message.content}")
+
+if __name__ == "__main__":
+    bot = Chat()
+    bot.run()
+
+
