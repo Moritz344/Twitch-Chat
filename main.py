4f86053d5be33ffb0303ade44a08001088b86810
Moritz <157284909+Moritz344@users.noreply.github.com>
2024-10-24 22:00:07 +0200

    Zählt Nachrichten und markiert user die viel schreiben

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#------------------------------------
+# Kommen zwei Stumme in die Bar. Sagt der eine:"",Sagt der andere:"".
+#------------------------------------
+
 from twitchio.ext import commands
 from dotenv import load_dotenv
 import os
@@ -7,14 +11,14 @@ from datetime import datetime
 
 load_dotenv("secret.env")
 Twitch_token = os.getenv("TOKEN")
-
+count = 0
 channel = None
+
 def main():
     os.system("clear")
-    print(colored("Streamer must be live.","yellow"))
-    print(colored("Press ctrl + c to quit.","red"))
+    print(colored("Works offline and online.", "yellow"))
+    print(colored("Press ctrl + c to quit.", "red"))
     channel = input("Type a Channel (Streamer) name: ")
-
     return channel
 
 channel = main()
@@ -24,9 +28,12 @@ class Chat(commands.Bot):
         super().__init__(token=Twitch_token, prefix=None, initial_channels=[channel])
 
     async def event_ready(self):
-        print(colored(f"Bot ist bereit und verbunden mit {self.nick} im Kanal {channel}","yellow"))
+        print(colored(f"Bot ist bereit und verbunden mit {self.nick} im Kanal {channel}", "yellow"))
 
     async def event_message(self, message):
+        global count
+        count += 1
+        # print(count,"minus char_counter ")
         farben_choice = ["red", "green", "cyan", "blue", "magenta"]
         self.farben = random.choice(farben_choice)
 
@@ -39,8 +46,21 @@ class Chat(commands.Bot):
         color_message = colored(f"{message.author.name}", self.farben)
         print(f"[{self.date_now}] {color_message}: {message.content}")
 
+        def pot_bibel_user(c):
+            char_counter = 0
+            for char in message.content:
+                char_counter += 1
+
+            if char_counter >= 25:
+                print(colored(char_counter, "red"), "Potenzieller Bibel Schreiber ", "--->", message.author.name)
+                c -= 1  # ich machen das damit die nachricht da oben nicht mitgezählt wird
+            return char_counter
+
+        char_counter = pot_bibel_user(count)
+
 if __name__ == "__main__":
     bot = Chat()
     bot.run()
 
+print(colored(f"\nMenge an Chat Nachrichten: {count}", "green"))
 
